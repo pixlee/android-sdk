ad27b87f55b13b96991befb0a5e7e52b929c4c9e
Andrew Barkett <andy@Andrews-iMac.local>
2017-04-16 23:28:04 -0700

    removed settings menu thing

--- app/src/main/java/com/pixlee/pixleeandroidsdk/SampleActivity.java
+++ app/src/main/java/com/pixlee/pixleeandroidsdk/SampleActivity.java
@@ -99,28 +99,6 @@ public class SampleActivity extends AppCompatActivity implements PXLAlbum.Reques
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sample, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /***
      * Callback for loadNextPageOfPhotos.  Update our list of photos and notify the adapters.
      * @param photos - the complete list of photos (both the latest page and all previous)
