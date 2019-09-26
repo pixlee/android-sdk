cfe1daf4951569b4cfb26ca1a26b45aba3b4b8df
Kyle Bennett <kylebennett@pixleeteam.com>
2019-09-26 17:11:09 -0400

    remove secret, proper sig calculation

--- app/src/main/java/com/pixlee/pixleeandroidsdk/SampleActivity.java
+++ app/src/main/java/com/pixlee/pixleeandroidsdk/SampleActivity.java
@@ -176,7 +176,7 @@ public class SampleActivity extends AppCompatActivity implements PXLAlbum.Reques
      */
     private void createAlbum() {
         Context c = this.getApplicationContext();
-        PXLClient.initialize("196i8ZzIAhKU8dO2kDe", "***REMOVED***");
+        PXLClient.initialize("196i8ZzIAhKU8dO2kDe");
         album = new PXLAlbum("4503434", c);
         PXLAlbumFilterOptions fo = new PXLAlbumFilterOptions();
         fo.minTwitterFollowers = 0;
@@ -239,7 +239,7 @@ public class SampleActivity extends AppCompatActivity implements PXLAlbum.Reques
         PXLAlbum.RequestHandlers rh = this;
         album.loadNextPageOfPhotos(rh);
 
-        album.uploadImage("test", "kb@pixleeteam.com", "kb", "https://cdn.pixabay.com/photo/2016/09/01/10/23/image-1635747_1280.jpg", true);
+        album.uploadImage("testttt", "kb@pixleeteam.com", "kb", "https://timedotcom.files.wordpress.com/2019/05/drake-nba-finals-warning.jpg", true);
 
 
     }
