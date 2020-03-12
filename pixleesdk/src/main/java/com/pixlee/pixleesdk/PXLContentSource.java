dafa67ce9a7f3d875b98e108e04f923578a42269
Sungjun <sungjun.app@gmail.com>
2020-03-12 15:18:44 +0900

    Fix a bug of making wrong parameters of '/album/{album id}'/photos according to this document: https://developers.pixlee.com/reference#consuming-content

--- pixleesdk/src/main/java/com/pixlee/pixleesdk/PXLContentSource.java
+++ pixleesdk/src/main/java/com/pixlee/pixleesdk/PXLContentSource.java
@@ -4,7 +4,8 @@ package com.pixlee.pixleesdk;
  * enum of the valid Pixlee content sources
  */
 public enum PXLContentSource {
-    INSTAGRAM ("instagram"),
+    INSTAGRAM_FEED ("instagram_feed"),
+    INSTAGRAM_STORY("instagram_story"),
     TWITTER ("twitter"),
     FACEBOOK ("facebook"),
     API ("api"),
