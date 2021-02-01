6e7d88eebc0ffae9feba164692997dccd4edf098
Sungjun <sungjun.app@gmail.com>
2021-02-01 16:51:35 +0900

    Remove a deprecated method

--- app/src/main/java/com/pixlee/pixleeandroidsdk/ui/widgets/ViewerActivity.kt
+++ app/src/main/java/com/pixlee/pixleeandroidsdk/ui/widgets/ViewerActivity.kt
@@ -66,11 +66,6 @@ class ViewerActivity : AppCompatActivity() {
         // set one of your region ids if you have.
         val regionId: Int? = null
 
-        // if you want to delegate firing `OpenLightbox` analytics event to PXLPhotoProductView, use this code.
-        // if you want to manually fire it, you don't use this and do need to implement our own analytics codes. Please check out KtxAnalyticsFragment.kt to get the sample codes.
-        // alternative: pxlPhotoProductView.enableAutoAnalytics(regionId)
-        pxlPhotoProductView.enableAutoAnalytics(regionId)
-
         // set your ui settings
         pxlPhotoProductView
                 .setContent(photoInfo = item,
