48b39517127b17468705bb7454d75704c5ead957
sungjun <sungjun.app@pixleeteam.com>
2021-04-26 12:53:08 +0900

    Add discount price information to PXLProduct

--- pixleesdk/src/main/java/com/pixlee/pixleesdk/data/PXLProduct.java
+++ pixleesdk/src/main/java/com/pixlee/pixleesdk/data/PXLProduct.java
@@ -4,11 +4,13 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import com.pixlee.pixleesdk.network.annotation.FieldBigDecimal;
+import com.pixlee.pixleesdk.network.annotation.FieldDate;
 import com.pixlee.pixleesdk.network.annotation.FieldURL;
 import com.squareup.moshi.Json;
 
 import java.math.BigDecimal;
 import java.net.URL;
+import java.util.Date;
 
 public class PXLProduct implements Parcelable {
     @Json(name = "id")
@@ -49,6 +51,18 @@ public class PXLProduct implements Parcelable {
     @Json(name = "price")
     public BigDecimal price;
 
+    @FieldDate
+    @Json(name = "sales_start_date")
+    public Date sales_start_date;
+
+    @FieldDate
+    @Json(name = "sales_end_date")
+    public Date sales_end_date;
+
+    @FieldBigDecimal
+    @Json(name = "sales_price")
+    public BigDecimal sales_price;
+
 
     public PXLProduct() {
     }
@@ -71,6 +85,28 @@ public class PXLProduct implements Parcelable {
         dest.writeString(this.description);
         dest.writeString(this.currency);
         dest.writeSerializable(this.price);
+        dest.writeLong(this.sales_start_date != null ? this.sales_start_date.getTime() : -1);
+        dest.writeLong(this.sales_end_date != null ? this.sales_end_date.getTime() : -1);
+        dest.writeSerializable(this.sales_price);
+    }
+
+    public void readFromParcel(Parcel source) {
+        this.id = source.readString();
+        this.link = (URL) source.readSerializable();
+        this.linkText = source.readString();
+        this.image = (URL) source.readSerializable();
+        this.imageThumb = (URL) source.readSerializable();
+        this.imageThumbSquare = (URL) source.readSerializable();
+        this.title = source.readString();
+        this.sku = source.readString();
+        this.description = source.readString();
+        this.currency = source.readString();
+        this.price = (BigDecimal) source.readSerializable();
+        long tmpSales_start_date = source.readLong();
+        this.sales_start_date = tmpSales_start_date == -1 ? null : new Date(tmpSales_start_date);
+        long tmpSales_end_date = source.readLong();
+        this.sales_end_date = tmpSales_end_date == -1 ? null : new Date(tmpSales_end_date);
+        this.sales_price = (BigDecimal) source.readSerializable();
     }
 
     protected PXLProduct(Parcel in) {
@@ -85,6 +121,11 @@ public class PXLProduct implements Parcelable {
         this.description = in.readString();
         this.currency = in.readString();
         this.price = (BigDecimal) in.readSerializable();
+        long tmpSales_start_date = in.readLong();
+        this.sales_start_date = tmpSales_start_date == -1 ? null : new Date(tmpSales_start_date);
+        long tmpSales_end_date = in.readLong();
+        this.sales_end_date = tmpSales_end_date == -1 ? null : new Date(tmpSales_end_date);
+        this.sales_price = (BigDecimal) in.readSerializable();
     }
 
     public static final Creator<PXLProduct> CREATOR = new Creator<PXLProduct>() {
